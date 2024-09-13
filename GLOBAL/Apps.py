7d64cad8511aa35e81b48d3f9d85a8b2ca778e13
André Ferreira <anfreire.dev@gmail.com>
2024-09-13 13:45:32 +0100

    13/09/2024

--- GLOBAL/Apps.py
+++ GLOBAL/Apps.py
@@ -1,58 +1,46 @@
-from typing import Any, Dict, Type, Protocol
+from typing import Any, Dict, Type, Protocol, List
 import json
 import importlib
-
+from dataclasses import dataclass
 
 class DynamicProviderClass(Protocol):
     def __init__(self, *args: Any): ...
-
     def __call__(self, *args: Any) -> str | None: ...
 
-
+@dataclass
 class Provider:
     class_name: str
-    init_args: list
-    call_args: list
+    init_args: List[Any]
+    call_args: List[Any]
 
-    def __init__(self, class_name: str, init_args: list, call_args: list):
-        self.class_name = class_name
-        self.init_args = init_args
-        self.call_args = call_args
-
-    def __get_dynamic_class(self) -> Type[DynamicProviderClass]:
+    def get_dynamic_class(self) -> Type[DynamicProviderClass]:
         module = importlib.import_module(f"Providers.{self.class_name}")
         return getattr(module, self.class_name)
 
     def __call__(self) -> str | None:
-        dynamic_class = self.__get_dynamic_class()
+        dynamic_class = self.get_dynamic_class()
         return dynamic_class(*self.init_args)(*self.call_args)
 
-
+@dataclass
 class App:
     name: str
     providers: Dict[str, Provider]
 
-    def __init__(self, name: str, providers: Dict[str, Provider]):
-        self.name = name
-        self.providers = providers
-
-    def __call__(self, providers: list[str] = None) -> None:
+    def __call__(self, providers: List[str] | None = None) -> None:
         from LIB.AppBase import AppBase
-
         app = AppBase(self.name, self.providers)
         app.update(providers)
-
 
 class Apps:
     def __init__(self, apps_json: str):
         self.apps: Dict[str, App] = {}
         self.apps_json = apps_json
-        self.__read(apps_json)
+        self.__read()
 
     def __getitem__(self, key: str) -> App:
         return self.apps[key]
 
-    def __setitem__(self, key: str, value: Any) -> None:
+    def __setitem__(self, key: str, value: App) -> None:
         self.apps[key] = value
 
     def __contains__(self, key: str) -> bool:
@@ -73,37 +61,37 @@ class Apps:
     def items(self):
         return self.apps.items()
 
-    def __read(self, apps_json: str) -> None:
-        with open(apps_json, "r") as file:
-            apps = json.load(file)
-        self.apps = {
-            app: App(
-                app,
-                {
-                    provider: Provider(
-                        provider_info["class"],
-                        provider_info["init_args"],
-                        provider_info["call_args"],
-                    )
-                    for provider, provider_info in providers.items()
-                },
-            )
-            for app, providers in apps.items()
-        }
+    def __read(self) -> None:
+        with open(self.apps_json, "r") as file:
+            apps_data = json.load(file)
+            self.apps = {
+                app_name: App(
+                    app_name,
+                    {
+                        provider_name: Provider(
+                            provider_info["class"],
+                            provider_info["init_args"],
+                            provider_info["call_args"],
+                        )
+                        for provider_name, provider_info in providers.items()
+                    },
+                )
+                for app_name, providers in apps_data.items()
+            }
 
     def write(self) -> None:
         with open(self.apps_json, "w") as file:
             json.dump(
                 {
-                    app: {
-                        provider: {
-                            "class": provider_info.class_name,
-                            "init_args": provider_info.init_args,
-                            "call_args": provider_info.call_args,
+                    app_name: {
+                        provider_name: {
+                            "class": provider.class_name,
+                            "init_args": provider.init_args,
+                            "call_args": provider.call_args,
                         }
-                        for provider, provider_info in app_info.providers.items()
+                        for provider_name, provider in app.providers.items()
                     }
-                    for app, app_info in self.apps.items()
+                    for app_name, app in self.apps.items()
                 },
                 file,
                 indent=4,
--- GLOBAL/Args.py
+++ GLOBAL/Args.py
@@ -1,52 +1,104 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import argparse
-from typing import List, Optional
+from typing import List, TypeVar, Generic, Literal
 
 
 @dataclass
-class Args:
-    app: Optional[List[str]] = None
-    github: bool = False
-    virustotal: bool = False
-    config: bool = False
+class BaseArgs:
     debug: bool = False
     xhost: bool = False
-    new_app: bool = False
-    new_provider: bool = False
-    provider: Optional[list[str]] = None
-    rm_icons: bool = False
-    rm_assets: bool = False
+
+    @staticmethod
+    def parse(parser: argparse.ArgumentParser) -> None:
+        parser.add_argument("-d", "--debug", action="store_true", help="Debug mode")
+        parser.add_argument(
+            "-x", "--xhost", action="store_true", help="Use xhost: Display"
+        )
+
+
+@dataclass
+class UpdateArgs(BaseArgs):
+    github: bool = False
+    virustotal: bool = False
+    apps: List[str] = field(default_factory=list)
+    providers: List[str] = field(default_factory=list)
+
+    @staticmethod
+    def parse(subparsers: argparse._SubParsersAction[argparse.ArgumentParser]) -> None:
+        update_parser = subparsers.add_parser(
+            "update", help="Update apps and providers"
+        )
+        update_parser.add_argument(
+            "-g", "--github", action="store_true", help="Force Github push"
+        )
+        update_parser.add_argument(
+            "-v", "--virustotal", action="store_true", help="Force VirusTotal analysis"
+        )
+        update_parser.add_argument("-a", "--apps", nargs="+", help="App name(s)")
+        update_parser.add_argument(
+            "-p", "--providers", nargs="+", help="Provider name(s)"
+        )
+
+
+@dataclass
+class NewArgs(BaseArgs):
+    app: bool = False
+    provider: bool = False
+
+    def parse(parser: argparse._SubParsersAction[argparse.ArgumentParser]) -> None:
+        new_parser = parser.add_parser("new", help="Add new app or provider")
+        new_parser.add_argument("-a", "--app", action="store_true", help="Add new app")
+        new_parser.add_argument(
+            "-p", "--provider", action="store_true", help="Add new provider"
+        )
+
+
+@dataclass
+class RemoveArgs(BaseArgs):
+    icons: bool = False
+    assets: bool = False
+
+    def parse(parser: argparse._SubParsersAction[argparse.ArgumentParser]) -> None:
+        remove_parser = parser.add_parser(
+            "remove", help="Remove unused icons or assets"
+        )
+        remove_parser.add_argument(
+            "-i", "--icons", action="store_true", help="Remove unused icons"
+        )
+        remove_parser.add_argument(
+            "-a", "--assets", action="store_true", help="Remove unused assets"
+        )
+
+
+T = TypeVar("T", UpdateArgs, NewArgs, RemoveArgs)
+
+Command = Literal["update", "new", "remove"]
+
+
+@dataclass
+class Args(Generic[T]):
+    command: Command
+    args: T
 
     @classmethod
     def parse_args(cls) -> "Args":
-        parser = argparse.ArgumentParser()
-        parser.add_argument("-a", "--app", "--apps", nargs="+", help="App name")
-        parser.add_argument(
-            "-g", "--github", help="Force Github push", action="store_true"
-        )
-        parser.add_argument(
-            "-v", "--virustotal", help="Force VirusTotal analysis", action="store_true"
-        )
-        parser.add_argument(
-            "-c", "--config", help="Edit config file", action="store_true"
-        )
-        parser.add_argument("-d", "--debug", help="Debug mode", action="store_true")
-        parser.add_argument(
-            "-x", "--xhost", help="Use xhost: Display", action="store_true"
-        )
-        parser.add_argument(
-            "-p", "--provider", "--providers", nargs="+", help="Provider name"
-        )
-        parser.add_argument("-na", "--new-app", help="Add new app", action="store_true")
-        parser.add_argument(
-            "-np", "--new-provider", help="Add new provider", action="store_true"
-        )
-        parser.add_argument(
-            "-ri", "--rm-icons", help="Remove unused icons", action="store_true"
-        )
-        parser.add_argument(
-            "-ra", "--rm-assets", help="Remove unused assets", action="store_true"
-        )
+        parser = argparse.ArgumentParser(description="Application argument parser")
+
+        BaseArgs.parse(parser)
+
+        subparsers = parser.add_subparsers(dest="command")
+
+        UpdateArgs.parse(subparsers)
+
+        NewArgs.parse(subparsers)
+
+        RemoveArgs.parse(subparsers)
 
         args = parser.parse_args()
-        return cls(**vars(args))
+
+        return cls(args.command, args)
+
+
+if __name__ == "__main__":
+    args = Args.parse_args()
+    print(args)
